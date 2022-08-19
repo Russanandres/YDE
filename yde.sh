63dba1ed124daebb64ff25c119362f2dfa2d4b6b
Russanandres <71694426+Russanandres@users.noreply.github.com>
2022-08-19 13:50:38 +0300

    Add files via upload

---
+++ yde.sh
@@ -0,0 +1,413 @@
+#!/bin/bash
+VER="0.9"
+startuptime=$(date)
+int=/usr/bin/runui
+source ~/.config/YDE/settings.conf
+
+### инициализация функций
+function pause(){ read -p "Press ENTER to continue...."
+}
+function exitscr(){
+ clear
+ echo Your Desktop Environment $VER - 2020-2022. Russanandres
+ date
+ exit
+}
+function loading() {
+    local i sp n
+    sp='/-\|'
+    n=${#sp}
+    printf ' '
+    while sleep 0.1; do
+        printf "%s\b" "${sp:i++%n:1}"
+    done
+}
+function start(){
+clear
+echo ┌──────────────────────── Homescreen ────────────────────────┐
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀┌───┐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀│⠀E⠀│⠀Exit YDE⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀└───┘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀┌───┐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀│⠀P⠀│⠀Poweroff⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀└───┘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀┌───┐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀│⠀D⠀│⠀Desktop⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀└───┘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[$(date +%D)] [$(date +%H:%M)]⠀│
+echo └────────────────────────────────────────────────────────────┘
+read ch
+case "$ch" in
+"E" )
+exitscr;;
+"e" )
+exitscr;;
+"P" )
+sudo poweroff;;
+"p" )
+sudo poweroff;;
+"D" )
+desktop;;
+"d" )
+desktop;;
+esac
+}
+
+
+# Аргументы запуска
+if [ "$1" == "-v" ]; then
+ echo $VER
+ exit
+elif [ "$1" == "--force-quit" ] || [ "$1" == "-fq" ]; then
+ exitscr
+elif [ "$@" != "--run" ]; then
+ echo Такого аргумента не существует! Используйте --commands для просмотра доступных аргументов.
+ echo Batus $VER
+ exit
+fi
+clear
+
+/tmp/updatingYDE.tmp
+
+if [ -f "/tmp/updatingYDE.tmp" ]; then
+clear
+else
+loading &
+sudo rm /usr/bin/runui
+sudo cp ./de.sh $int
+sudo chmod +x $int
+sleep 2
+kill "$!"
+fi
+
+
+if [ -f "$int" ]; then
+clear
+echo Loading Your Desktop Environment...
+echo
+loading &
+sleep 1
+kill "$!"
+while true; do
+function desktop(){
+clear
+echo ┌─────────────────────── Your Desktop ───────────────────────┐
+echo │ [1] - About YDE⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │ [2] - Back to Command line⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │ [3] - Environment settings⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │ [S] Homescreen⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[$(date +%D)] [$(date +%H:%M)]⠀│
+echo └────────────────────────────────────────────────────────────┘
+read ch
+case "$ch" in
+"S" )
+start;;
+"s" )
+start;;
+"1" )
+about;;
+"2" )
+exitscr;;
+"3" )
+settings;;
+esac
+}
+function about(){
+clear
+echo ┌─────────────────────── About Desktop ──────────────────────┐
+echo │ Your Desktop Environment⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │ Version $VER ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│
+echo │ [S] Homescreen⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[$(date +%D)] [$(date +%H:%M)]⠀│
+echo └────────────────────────────────────────────────────────────┘
+read ch
+case "$ch" in
+"S" )
+start;;
+"s" )
+start;;
+"D" )
+desktop;;
+"d" )
+desktop;;
+esac
+}
+function settings(){
+clear
+echo ┌───────────────────── Desktop Settings ─────────────────────┐
+echo │ [T] Theme - $THEME
+echo │ [-] Installed - $inst
+echo │
+echo │
+echo │ [U] Check Update
+echo │ [Q] Reinstall from de.sh
+echo │ [R] Remove YDE
+echo │
+echo │
+echo │
+echo │
+echo │
+echo │
+echo │ [S] Homescreen⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[$(date +%D)] [$(date +%H:%M)]⠀│
+echo └────────────────────────────────────────────────────────────┘
+read ch
+case "$ch" in
+"T" )
+themes;;
+"U" )
+update;;
+"R" )
+remove;;
+"Q" )
+reinstall;;
+"t" )
+themes;;
+"u" )
+update;;
+"r" )
+remove;;
+"q" )
+reinstall;;
+"s" )
+start;;
+"S" )
+start;;
+"D" )
+desktop;;
+"d" )
+desktop;;
+esac
+}
+function themes(){
+clear
+echo ┌────────────────────── Theme Settings ──────────────────────┐
+echo │ Active theme - $THEME
+echo │
+echo │
+echo │
+echo │
+echo │
+echo │
+echo │
+echo │
+echo │
+echo │
+echo │
+echo │
+echo │ [S] Homescreen⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[$(date +%D)] [$(date +%H:%M)]⠀│
+echo └────────────────────────────────────────────────────────────┘
+}
+
+
+function update(){
+clear
+clear
+echo
+echo ⠀⠀⠀┌──────────────────────────────────────────────────────────────────────────────────────────────────────┐
+echo ⠀⠀ │YDE Updater⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[part 1 of 2]⠀│
+echo ⠀⠀⠀└──────────────────────────────────────────────────────────────────────────────────────────────────────┘
+echo
+echo
+echo ⠀⠀⠀⠀⠀⠀Welcome to the YDE update system.
+echo
+echo ⠀⠀⠀⠀⠀⠀We are downloading the latest version from the github site on the official repository.
+echo ⠀⠀⠀⠀⠀⠀Please check your internet connection.
+echo
+echo ⠀⠀⠀⠀⠀⠀⠀- Press ENTER to update.
+echo ⠀⠀⠀⠀⠀⠀⠀- Press CTRL + C to exit.
+echo
+pause
+clear
+echo
+echo ⠀⠀⠀┌──────────────────────────────────────────────────────────────────────────────────────────────────────┐
+echo ⠀⠀ │YDE Installation⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[part 2 of 2]⠀│
+echo ⠀⠀⠀└──────────────────────────────────────────────────────────────────────────────────────────────────────┘
+echo
+echo
+echo ⠀⠀⠀⠀⠀⠀We updating your Desktop Environment...
+loading &
+touch /tmp/updatingYDE.tmp
+wget
+bash de.sh
+}
+
+
+
+
+
+
+function reinstall(){
+clear
+echo
+echo ⠀⠀⠀┌──────────────────────────────────────────────────────────────────────────────────────────────────────┐
+echo ⠀⠀ │YDE reInstallation⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[part 1 of 1]⠀│
+echo ⠀⠀⠀└──────────────────────────────────────────────────────────────────────────────────────────────────────┘
+echo
+echo
+echo ⠀⠀⠀⠀⠀⠀Hello!
+echo
+echo ⠀⠀⠀⠀⠀⠀We are reinstalling your YDE.
+echo ⠀⠀⠀⠀⠀⠀Please wait.
+loading &
+sudo rm /usr/bin/runui
+sudo cp ./de.sh $int
+sudo chmod +x $int
+sleep 2
+kill "$!"
+exitscr
+}
+
+
+function remove(){
+clear
+echo
+echo ⠀⠀⠀┌──────────────────────────────────────────────────────────────────────────────────────────────────────┐
+echo ⠀⠀ │YDE deInstallation⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[part 1 of 3]⠀│
+echo ⠀⠀⠀└──────────────────────────────────────────────────────────────────────────────────────────────────────┘
+echo
+echo
+echo ⠀⠀⠀⠀⠀⠀Hello!
+echo
+echo ⠀⠀⠀⠀⠀⠀We are very sorry that you have to uninstall YDE.
+echo ⠀⠀⠀⠀⠀⠀If you have any problems, please create an issue on github.
+echo
+echo ⠀⠀⠀⠀⠀⠀⠀- Press ENTER to uninstall.
+echo ⠀⠀⠀⠀⠀⠀⠀- Press CTRL + C to exit.
+echo
+pause
+clear
+echo
+echo ⠀⠀⠀┌──────────────────────────────────────────────────────────────────────────────────────────────────────┐
+echo ⠀⠀ │YDE deInstallation⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[part 2 of 3]⠀│
+echo ⠀⠀⠀└──────────────────────────────────────────────────────────────────────────────────────────────────────┘
+echo
+echo
+echo ⠀⠀⠀⠀⠀⠀We uninstalling YDE. Please wait.
+loading &
+sudo rm /usr/bin/runui
+sudo rm -rf ~/.config/YDE
+rm -rf ~/.config/YDE
+sleep 2
+kill "$!"
+clear
+echo
+echo ⠀⠀⠀┌──────────────────────────────────────────────────────────────────────────────────────────────────────┐
+echo ⠀⠀ │YDE deInstallation⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[part 3 of 3]⠀│
+echo ⠀⠀⠀└──────────────────────────────────────────────────────────────────────────────────────────────────────┘
+echo
+echo
+echo ⠀⠀⠀⠀⠀⠀YDE has been successfully uninstalled on your PC!
+echo
+echo ⠀⠀⠀⠀⠀⠀We are waiting for you again!
+echo ⠀⠀⠀⠀⠀⠀The uninstaller will close in a couple of seconds.
+echo
+sleep 3
+exitscr
+
+}
+
+
+
+
+
+
+
+
+desktop
+pause
+
+
+
+
+
+
+
+
+
+
+
+done
+else
+### screen one
+clear
+echo
+echo ⠀⠀⠀┌──────────────────────────────────────────────────────────────────────────────────────────────────────┐
+echo ⠀⠀ │YDE Installation⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[part 1 of 3]⠀│
+echo ⠀⠀⠀└──────────────────────────────────────────────────────────────────────────────────────────────────────┘
+echo
+echo
+echo ⠀⠀⠀⠀⠀⠀Welcome to the first boot of YDE!
+echo
+echo ⠀⠀⠀⠀⠀⠀This pseudo-graphical shell will try to make you comfortable in the terminal.
+echo ⠀⠀⠀⠀⠀⠀The environment will now be installed on your computer.
+echo
+echo ⠀⠀⠀⠀⠀⠀⠀- Press ENTER to continue
+echo ⠀⠀⠀⠀⠀⠀⠀- Press CTRL + C to exit.
+echo
+pause
+
+### screen three
+clear
+echo
+echo ⠀⠀⠀┌──────────────────────────────────────────────────────────────────────────────────────────────────────┐
+echo ⠀⠀ │YDE Installation⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[part 2 of 3]⠀│
+echo ⠀⠀⠀└──────────────────────────────────────────────────────────────────────────────────────────────────────┘
+echo
+echo
+echo ⠀⠀⠀⠀⠀⠀We installing the required parts...
+loading &
+sudo cp ./de.sh $int
+sudo chmod +x $int
+mkdir ~/.config/YDE/
+mkdir ~/.config/YDE/themes
+mkdir ~/.config/YDE/old
+touch ~/.config/YDE/settings.conf
+echo THEME=classic >> ~/.config/YDE/settings.conf
+echo $(sudo apt install ncal dialog -y) >> /dev/null
+sleep 2
+kill "$!"
+
+### screen four
+clear
+echo
+echo ⠀⠀⠀┌──────────────────────────────────────────────────────────────────────────────────────────────────────┐
+echo ⠀⠀ │YDE Installation⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀[part 3 of 3]⠀│
+echo ⠀⠀⠀└──────────────────────────────────────────────────────────────────────────────────────────────────────┘
+echo
+echo
+echo ⠀⠀⠀⠀⠀⠀YDE has been successfully installed on your PC!
+echo
+echo ⠀⠀⠀⠀⠀⠀Please write runui to run YDE.
+echo ⠀⠀⠀⠀⠀⠀The installer will close in a couple of seconds.
+echo
+sleep 3
+ clear
+ echo run "runui" to run shell.
+ echo
+ echo Your Desktop Environment $VER - 2020-2022. Russanandres
+ date
+ exit
+fi
