ed0b1b7f9b18279eee85d45900b62e13c1aa04be
Russanandres <71694426+Russanandres@users.noreply.github.com>
2022-09-12 14:54:29 +0300

    Add files via upload

---
+++ YDE_fallback.sh
@@ -0,0 +1,327 @@
+#!/bin/bash
+VER="1.0"
+startuptime=$(date)
+int=/usr/bin/safeui
+# source ~/.config/YDE/settings.conf
+source ~/.config/YDE/fallback.conf
+# source ~/.config/YDE/yde.log
+function pause(){ read -p "Press ENTER to continue...."
+}
+function exitscr(){
+ clear
+ echo Your Desktop Environment $VER - 2021-2022. Russanandres
+ date
+ exit
+}
+function loading() {
+    local i sp n
+    sp='/-\|'
+    n=${#sp}
+    printf ' '
+    while sleep 0.1; do
+        printf "%s\b" "${sp:i++%n:1}"
+    done
+}
+trap "exitscr" SIGINT
+if [ -f "$int" ]; then inst="true "; else inst=false; fi
+if [ "$1" == "-v" ]; then echo $VER; exit
+elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+ echo Your Desktop Environment Fallback Mode
+ echo Shell TTY DE
+ echo If you have good pc, please download normal verison.
+ echo
+ echo Avialible commands:
+ echo To try YDE without installion run with -p or --portable
+ echo To check version run with -v or --version
+ echo To force quit run with -fq or --force-quit
+ echo To see all arguments run with --help
+ exit
+elif [ "$1" == "--force-quit" ] || [ "$1" == "-fq" ]; then exitscr
+elif [ "$1" == "-p" ] || [ "$1" == "--portable" ]; then portable=true
+elif [ "$@" != "--run" ]; then
+ echo This run argument is not exist! Use --help to see all avialible arguments.
+ echo YDE $VER
+ exit
+fi
+clear
+if [ -f "$int" ] || [ "$1" == "-p" ] || [ "$1" == "--portable" ]; then
+clear
+echo Loading Your Desktop Environment...
+echo
+loading &
+source /usr/share/YDE/settings.conf
+printf '\033[8;17;63t'
+screen=startingup
+if [ "$fastboot" == "true" ] || [ "$1" == "-f" ] || [ "$2" == "-f" ]; then
+break
+else
+sleep 1
+fi
+kill "$!"
+while true; do
+function start(){
+screen=start
+clear
+echo "┌──────────────────────── Homescreen ────────────────────────┐"
+echo "│                                                            │"
+echo "│  ┌───┐                                                     │"
+echo "│  │ E │ Exit YDE                                            │"
+echo "│  └───┘                                                     │"
+echo "│  ┌───┐                                                     │"
+echo "│  │ P │ Poweroff                                            │"
+echo "│  └───┘                                                     │"
+echo "│  ┌───┐                                                     │"
+echo "│  │ D │ Desktop                                             │"
+echo "│  └───┘                                                     │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│ [S] Homescreen                          [$(date +%D)] [$(date +%H:%M)] │"
+echo "└────────────────────────────────────────────────────────────┘"
+read -sn1 ch
+case "$ch" in
+"E"|"e" ) exitscr;;
+"P"|"p" ) sudo poweroff;;
+"D"|"d" ) desktop;;
+"S"|"s" ) desktop;;
+esac
+}
+function desktop(){
+screen=desktop
+clear
+echo "┌─────────────────────── Your Desktop ───────────────────────┐"
+echo "│ [1] - About YDE                                            │"
+echo "│ [2] - Back to Command line                                 │"
+echo "│ [3] - Environment settings                                 │"
+echo "│                                                            │"
+echo "│                   ___  _ ____  _____                       │"
+echo "│                   \  \///  _ \/  __/                       │"
+echo "│                    \  / | | \||  \                         │"
+echo "│                    / /  | |_/||  /_                        │"
+echo "│                   /_/   \____/\____\                       │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│                                             [c] - Clock    │"
+echo "│ [S] Homescreen                          [$(date +%D)] [$(date +%H:%M)] │"
+echo "└────────────────────────────────────────────────────────────┘"
+read -sn1 ch
+case "$ch" in
+"S"|"s" ) start;;
+"C"|"c" ) clock;;
+"1" ) about;;
+"2" ) exitscr;;
+"3" ) settings;;
+esac
+}
+function about(){
+screen=about
+clear
+echo "┌─────────────────────── About Desktop ──────────────────────┐"
+echo "│ Your Desktop Environment                                   │"
+echo "│ Version $VER                                                │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│                   ___  _ ____  _____                       │"
+echo "│                   \  \///  _ \/  __/                       │"
+echo "│                    \  / | | \||  \                         │"
+echo "│                    / /  | |_/||  /_                        │"
+echo "│                   /_/   \____/\____\                       │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│ [S] Homescreen                          [$(date +%D)] [$(date +%H:%M)] │"
+echo "└────────────────────────────────────────────────────────────┘"
+read -sn1 ch
+case "$ch" in
+"S"|"s" ) start;;
+"D"|"d" ) desktop;;
+esac
+}
+function settings(){
+screen=settings
+clear
+echo "┌───────────────────── Desktop Settings ─────────────────────┐"
+echo "│                                                            │"
+echo "│ [-] Installed - $inst                                      │"
+echo "│                                                            │"
+echo "│ [Q] Reinstall from YDE_fallback.sh                         │"
+echo "│ [R] Remove YDE                                             │"
+echo "│                                                            │"
+echo "│ [I] Install classic version                                │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│ [S] Homescreen                          [$(date +%D)] [$(date +%H:%M)] │"
+echo "└────────────────────────────────────────────────────────────┘"
+read -sn1 ch
+case "$ch" in
+"T"|"t" ) themes;;
+"R"|"r" ) remove;;
+"Q"|"q" ) reinstall;;
+"S"|"s" ) start;;
+"D"|"d" ) desktop;;
+"I"|"i" ) curl -s https://raw.githubusercontent.com/Russanandres/YDE/main/de.sh | bash;;
+esac
+}
+function clock(){
+clear
+while sleep 1; do
+clear
+echo "┌─────────────────────────── Clock ──────────────────────────┐"
+echo "│                                                            │"
+echo "│      Now is:                                               │"
+echo "│  $(date +%D) $(date +%H:%M:%S)                                         │"
+echo "│         $(date +%a)                                                 │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│                   ___  _ ____  _____                       │"
+echo "│                   \  \///  _ \/  __/                       │"
+echo "│                    \  / | | \||  \                         │"
+echo "│                    / /  | |_/||  /_                        │"
+echo "│                   /_/   \____/\____\                       │"
+echo "│                                                            │"
+echo "│                                                            │"
+echo "│ Press CTRL + C to exit                                     │"
+echo "└────────────────────────────────────────────────────────────┘"
+trap "desktop" EXIT
+done
+}
+function reinstall(){
+printf '\033[8;17;66t'
+screen=reinstall
+clear
+echo
+echo "   ┌────────────────────────────────────────────────────────────┐"
+echo "   │ YDEf reInstallion                            [part 1 of 1] │"
+echo "   └────────────────────────────────────────────────────────────┘"
+echo
+echo
+echo "      Hello!"
+echo
+echo "      We are reinstalling your YDE fallback."
+echo "      Please wait."
+loading &
+sudo rm $int
+sudo cp ./YDE_fallback.sh $int
+sudo chmod +x $int
+sleep 2
+kill "$!"
+exitscr
+}
+function remove(){
+printf '\033[8;17;66t'
+screen=remove
+clear
+echo
+echo "   ┌────────────────────────────────────────────────────────────┐"
+echo "   │ YDEf deInstallion                            [part 1 of 3] │"
+echo "   └────────────────────────────────────────────────────────────┘"
+echo
+echo
+echo "      Hello!"
+echo
+echo "      We are very sorry that you have to uninstall YDE fallback."
+echo "      If you have any problems, please create an issue on github."
+echo
+echo "       - Press ENTER to uninstall."
+echo "       - Press CTRL + C to exit."
+echo
+pause
+clear
+echo
+echo "   ┌────────────────────────────────────────────────────────────┐"
+echo "   │ YDEf deInstallion                            [part 2 of 3] │"
+echo "   └────────────────────────────────────────────────────────────┘"
+echo
+echo
+echo "   We uninstalling YDE fallback. Please wait."
+loading &
+sudo rm $int
+rm -f ~/.config/YDE/fallback.conf
+sleep 2
+kill "$!"
+clear
+echo
+echo "   ┌────────────────────────────────────────────────────────────┐"
+echo "   │ YDEf deInstallion                            [part 3 of 3] │"
+echo "   └────────────────────────────────────────────────────────────┘"
+echo
+echo
+echo "      YDE fallback has been successfully uninstalled on your PC!"
+echo
+echo "      We are waiting for you again!"
+echo "      The uninstaller will close in a couple of seconds."
+echo
+sleep 3
+exitscr
+}
+if [ "$screen" == "startingup" ]; then desktop; else $screen; fi
+done
+else
+printf '\033[8;17;66t'
+clear
+echo
+echo "   ┌────────────────────────────────────────────────────────────┐"
+echo "   │ YDEf Installion                               [part 1 of 3] │"
+echo "   └────────────────────────────────────────────────────────────┘"
+echo
+echo
+echo "      Welcome to the first boot of YDE fallback!"
+echo
+echo "      This pseudo-graphical shell will try to make you comfortable in the terminal."
+echo "      The environment will now be installed on your computer."
+echo
+echo "       - Press ENTER to continue"
+echo "       - Press CTRL + C to exit."
+echo
+pause
+clear
+echo
+echo "   ┌────────────────────────────────────────────────────────────┐"
+echo "   │ YDEf Installion                               [part 2 of 3] │"
+echo "   └────────────────────────────────────────────────────────────┘"
+echo
+echo
+echo "    We installing the required parts..."
+echo
+loading &
+sudo cp ./YDE_fallback.sh $int
+sudo chmod +x $int
+sudo mkdir /usr/share/YDE
+mkdir ~/.config/YDE/
+mkdir ~/.config/YDE/themes
+mkdir ~/.config/YDE/languages
+mkdir ~/.config/YDE/old
+# touch ~/.config/YDE/yde.log
+touch ~/.config/YDE/fallback.conf
+sudo touch /usr/share/YDE/fallback.conf
+touch ~/.config/YDE/apps/list
+echo THEME=classic >> ~/.config/YDE/fallback.conf
+echo int=$int >> ~/.config/YDE/fallback.conf
+sleep 2
+kill "$!"
+clear
+echo
+echo "   ┌────────────────────────────────────────────────────────────┐"
+echo "   │ YDEf Installion                               [part 3 of 3] │"
+echo "   └────────────────────────────────────────────────────────────┘"
+echo
+echo
+echo "      YDE fallback has been successfully installed on your PC!"
+echo
+echo "      Please write safeui to run YDEf."
+echo "      The installer will close in a couple of seconds."
+echo
+sleep 3
+ clear
+ echo "run safeui to run shell."
+ echo
+ echo "Your Desktop Environment Fallback $VER - 2021-2022. Russanandres"
+ date
+ exit
+fi
